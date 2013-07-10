d7fbd918ec27af3aa8cb992b59f1d514f63d85d4
Yang Hong <hy.styx@gmail.com>
2013-07-10 20:06:09 +0800

    modify test_pipe.c
    not using mutex to protect reads from pipe

--- snippets/test_pipe.c
+++ snippets/test_pipe.c
@@ -14,29 +14,43 @@
 #include <pthread.h>
 #include <sched.h>                      /* pthread_yield missing on OSX */
 
+#define cpu_relax() asm volatile("pause\n": : :"memory")
+
 int fd0[2];
 int fd1[2];
 
 pthread_t tid[2];
 pthread_t disp;
 
+pthread_mutex_t g_mtx;
+
 void *readpipe(void *arg)
 {
     static int nthreads = 0;
     int id = __sync_fetch_and_add(&nthreads, 1);
-    int *fd = (id == 0)?fd0:fd1;
+    /* int *fd = (id == 0)?fd0:fd1; */
+    int *fd = fd0;
 
     int buf;
+    struct stat st;
     printf("thread %d\n", id);
     while (1) {
+        /* pthread_mutex_lock(&g_mtx); */
         int n = read(fd[0], &buf, sizeof(int));
+        /* pthread_mutex_unlock(&g_mtx); */
         if (n == sizeof(int)) {
             printf("%d-%d\n", id, buf);
-            memset(&buf, 0, sizeof(int));
+            /* memset(&buf, 0, sizeof(int)); */
+            fstat(buf, &st);
+            printf("st_ino: %d\n", st.st_ino);
+            close(buf);
         } else if (n == 1 && buf == 0) {
             break;
+        } else if (n == 0) {
+            break;
         }
-        sched_yield();
+        /* sched_yield(); */
+        cpu_relax();
     }
     pthread_exit(0);
 }
@@ -72,15 +86,18 @@ void *dispatcher(void *arg)
             dprintf(INFO, "open file: %s\n", fullpath);
             if (write(fd0[1], &fd, sizeof(int)) != sizeof(int)) {
                 perror("write fd0\n");
-                if (write(fd1[1], &fd, sizeof(int)) != sizeof(int)) {
-                    perror("write fd1\n");
-                }
+                /* if (write(fd1[1], &fd, sizeof(int)) != sizeof(int)) { */
+                /*     perror("write fd1\n"); */
+                /* } */
             }
             sched_yield();
         }
     }
     write(fd0[1], "\0", 1);
-    write(fd1[1], "\0", 1);
+    write(fd0[1], "\0", 1);
+    /* write(fd1[1], "\0", 1); */
+    close(fd0[1]);
+    close(fd1[1]);
 
     if (closedir(d) < 0) {
         dprintf(ERROR, "closedir failed");
@@ -91,6 +108,7 @@ void *dispatcher(void *arg)
 
 int main(int argc, char *argv[])
 {
+    pthread_mutex_init(&g_mtx, NULL);
     if (pipe(fd0) == -1) {
         perror("pipe");
         exit(EXIT_FAILURE);
@@ -107,9 +125,10 @@ int main(int argc, char *argv[])
     pthread_join(tid[0], NULL);
     pthread_join(tid[1], NULL);
     close(fd0[0]);
-    close(fd0[1]);
+    /* close(fd0[1]); */
     close(fd1[0]);
-    close(fd1[1]);
+    /* close(fd1[1]); */
+    pthread_mutex_destroy(&g_mtx);
     return 0;
 }
 
